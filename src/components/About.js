773456930616ed7a36bba3dd642e81f4dd9897d9
Thamizharasan Devaraj <cdkthamizh27@gmail.com>
2020-12-12 18:48:03 +0530

    Added About Page info

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
+import memoji from "../img/memoji.PNG";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const About = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 150,
+      delay: 700,
+      once: true,
+      easing: "ease",
+    });
+    document.title = "About - Thamizh's Portfolio";
+  });
+
   return (
-    <div>
-      <h1>About Page</h1>
+    <div className="mt-20">
+      <div>
+        <img
+          src={memoji}
+          alt="memoji"
+          className="w-60 mx-auto relative top-12 rounded-full"
+          data-aos="fade-down"
+        />
+      </div>
+      <div className="text-xl w-5/12 m-auto text-center mt-24 leading-8">
+        <p>
+          I'm a self-taught Front End Developer based in
+          <span className="font-semibold text-blue-800">
+            {" "}
+            Tamilnadu, India.
+          </span>
+        </p>
+        <p>
+          Passionate about client-side development. And focusing on fullstack
+          development. I easily bored often. but, development is one of the
+          things I don't bored. I'm a developer who focuses on writing clean,
+          elegent and efficient code.{" "}
+          <q className="text-base block text-gray-600">
+            Anyone can write code. but, not everyone can write clean code.
+          </q>
+        </p>
+      </div>
+      <div className="mt-20 border-t-2 border-gray-50 text-center text-2xl">
+        <p className="p-5">&copy; Copyright Thamizh 2020</p>
+      </div>
     </div>
   );
 };
