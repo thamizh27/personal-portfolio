998df328613f389f544f2444d4a9fbed29f89210
Thamizharasan Devaraj <cdkthamizh27@gmail.com>
2020-12-15 11:57:45 +0530

    Adding state for NavBar and SideBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import NavBar from "./components/NavBar";
+import SideBar from "./components/SideBar";
 
 function App() {
+  const [open, setOpen] = useState(false);
+
+  const toggle = () => {
+    setOpen(!open);
+  };
+
   return (
     <BrowserRouter>
-      <NavBar />
+      <SideBar open={open} toggle={toggle} />
+      <NavBar toggle={toggle} />
       <Switch>
         <Route component={Home} path="/" exact />
         <Route component={Projects} path="/projects" />
