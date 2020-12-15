245bc2d8d22225dfe65c183747661869567d6516
Thamizharasan Devaraj <cdkthamizh27@gmail.com>
2020-12-15 11:58:23 +0530

    Adding menu icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,9 +3,7 @@ import { NavLink } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-const NavBar = () => {
-  // const [isOpen, setIsOpen] = useState(false);
-
+const NavBar = ({ toggle }) => {
   useEffect(() => {
     AOS.init({
       duration: 200,
@@ -13,16 +11,13 @@ const NavBar = () => {
       once: true,
       easing: "ease",
     });
-  });
+  }, []);
 
   return (
     <div>
-      <div className="font-sans text-lg lg:text-2xl bg-white z-10 border-b-2 border-gray-300 shadow-sm fixed top-0 left-0 right-0">
+      <div className="font-sans text-lg md:text-2xl bg-white z-10 border-b-2 border-gray-300 shadow-sm fixed top-0 left-0 right-0">
         <nav className="m-auto h-20 w-full md:w-11/12  flex justify-between items-center">
-          <div
-            className="font-semibold tracking-wider relative"
-            data-aos="zoom-in"
-          >
+          <div className="font-semibold tracking-wider" data-aos="zoom-in">
             <NavLink className="p-4" to="/" exact>
               <span className="text-blue-800">&lt;</span>
               Thamizh
@@ -30,24 +25,27 @@ const NavBar = () => {
               <span className="text-blue-800">&gt;</span>
             </NavLink>
           </div>
-          <div className="text-base md:text-lg lg:text-2xl flex right-8">
+          <div className="lg:hidden mr-4 cursor-pointer" onClick={toggle}>
+            <i className="fas fa-bars text-3xl"></i>
+          </div>
+          <div className="hidden lg:flex right-8">
             <NavLink
               activeClassName="text-white bg-blue-800 rounded-full"
-              className="px-2 py-1 md:mr-4 md:px-4 md:py-2 block"
+              className="mr-4 px-4 py-2 block"
               to="/projects"
             >
               Projects
             </NavLink>
             <NavLink
               activeClassName="text-white bg-blue-800 rounded-full"
-              className=" px-2 py-1 md:mr-4 md:px-4 md:py-2 block"
+              className="mr-4 px-4 py-2 block"
               to="/about"
             >
               About
             </NavLink>
             <NavLink
               activeClassName="text-white bg-blue-800 rounded-full"
-              className="mr-1 px-2 py-1 md:mr-4 md:px-4 md:py-2 block"
+              className="mr-4 px-4 py-2 block"
               to="contact"
             >
               Contact
